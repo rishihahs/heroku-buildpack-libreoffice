5fa858a74fce2cc5a702ba826bb6d00e67111146
Rishi Shah <rishihahs@gmail.com>
2013-12-25 21:45:04 -0600

    Rename

--- .profile.d/config_vars.sh
+++
@@ -1,5 +0,0 @@
-# add vendor binaries to the path
-export PATH=$PATH:/app/vendor/libreoffice/program:/app/vendor/libreoffice/deps/bin
-
-# configure LD_LIBRARY_PATH to include dependencies
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/app/vendor/libreoffice/deps/lib
---
+++ .profile.d/heroku-buildpack-libreoffice.sh
@@ -0,0 +1,5 @@
+# add vendor binaries to the path
+export PATH="$HOME/vendor/libreoffice/program:/app/vendor/libreoffice/deps/bin:$PATH"
+
+# configure LD_LIBRARY_PATH to include dependencies
+export LD_LIBRARY_PATH="$HOME/vendor/libreoffice/deps/lib:$LD_LIBRARY_PATH"
